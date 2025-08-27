47d4b0950ed412a6aeccab2c39c553f98d3bab4b
Suyash Acharya <suyashacharya369@gmail.com>
2025-08-27 19:11:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//Project: Print Details from an Object :
+let student = {
+    name : "Suyash",
+    age : 20,
+    course : "BCA",
+    branch : "AIML",
+    college : "Scope Global Skills University"
+};
+
+console.log("#######__My-Details__#######");
+console.log("Name : " + student.name);
+console.log("Age : " + student.age);
+console.log("Course : " + student.course);
+console.log("Branch : " + student.branch);
+console.log("College :" + student.college);
